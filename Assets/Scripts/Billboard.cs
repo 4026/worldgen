dc7332a8f5a4159df0dfd9dad164eb8d90ef1b40
Ben Hallett <name4026@gmail.com>
2016-07-31 18:50:49 +0100

    Added a position marker
    (with a little bit of bounce on spawn, hence LeanTween)

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Component that makes the GameObject always face towards the camera along the xz plane (that is, 
+/// it ignores the camera's altitude).
+/// </summary>
+public class Billboard : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //Face the camera
+        Vector3 camera_direction = transform.position - Camera.main.transform.position;
+        camera_direction.y = 0;
+        transform.rotation = Quaternion.LookRotation(camera_direction, Vector3.up);
+    }
+}
--- Assets/Scripts/TerrainUIHandler.cs
+++ Assets/Scripts/TerrainUIHandler.cs
@@ -6,11 +6,14 @@ using System;
 
 public class TerrainUIHandler : MonoBehaviour, IPointerClickHandler
 {
-    Text m_locationText;
+    private Text m_locationText;
+    private GameObject m_positionMarker;
+    private GameObject m_positionMarkerPrefab;
 
     public void Start()
     {
         m_locationText = GameObject.Find("LocationText").GetComponent<Text>();
+        m_positionMarkerPrefab = Resources.Load("Prefabs/PositionMarker") as GameObject;
     }
 
 	/// <summary>
@@ -24,10 +27,28 @@ public class TerrainUIHandler : MonoBehaviour, IPointerClickHandler
             return;
         }
 
-		TerrainGenerator generator = GetComponent<TerrainGenerator> ();
-		Point heightmapPos = generator.GetHeightmapPosFromWorldPos (eventData.pointerCurrentRaycast.worldPosition);
+        //Get info about clicked point.
+        Vector3 clicked_point = eventData.pointerCurrentRaycast.worldPosition;
+        TerrainGenerator generator = GetComponent<TerrainGenerator> ();
+		Point heightmapPos = generator.GetHeightmapPosFromWorldPos (clicked_point);
 		BiomeMapPixel biomeData = generator.BiomeMap.GetPixelAtPoint(heightmapPos);
 
+        //Place location marker.
+        if (m_positionMarker != null)
+        {
+            Destroy(m_positionMarker);
+        }
+        Vector3 camera_direction = transform.position - Camera.main.transform.position;
+        camera_direction.y = 0;
+        Quaternion look_rotation = Quaternion.LookRotation(camera_direction, Vector3.up);
+        m_positionMarker = Instantiate(m_positionMarkerPrefab, clicked_point, look_rotation) as GameObject;
+
+        //Animate it into position.
+        m_positionMarker.transform.localScale = new Vector3(1, 0, 0);
+        LeanTween.scaleY(m_positionMarker, 1, 0.5f).setEase(LeanTweenType.easeOutElastic);
+        LeanTween.scaleZ(m_positionMarker, 1, 1f).setEase(LeanTweenType.easeOutElastic);
+
+        //Update location text
         m_locationText.text = heightmapPos + Environment.NewLine + biomeData;
 	}
 }
