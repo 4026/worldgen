0b9c8c4cbd60cade7c5fab8f2c71114ec299a383
Ben Hallett <name4026@gmail.com>
2016-08-08 22:21:24 +0100

    Added the ability to quit the application by pressing Esc

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	void Update ()
+    {
+	    if (Input.GetButtonUp("Cancel"))
+        {
+            Application.Quit();
+        }
+	}
+}
